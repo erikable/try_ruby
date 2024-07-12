7510756ed5dcc1b979ba2d6bf46f522149a59f4d
erikable <erik.surmilo@gmail.com>
2024-07-12 12:12:59 +0300

    update simple calc

--- simple_calc.rb
+++ simple_calc.rb
@@ -1,5 +1,16 @@
+puts "Enter operator (add/minus/milt/devide/root/area)"
+oper = gets.chomp.downcase
+
+puts "Enter number"
+n1 = gets.chomp.to_f
+
+if oper != 'root' && oper != 'area'
+  puts "Enter second number"
+  n2 = gets.chomp.to_f
+end
+
 def display(num)
-  sprintf("%.2f", num)
+  puts "Result is = #{sprintf("%.2f", num)}"
 end
 
 def add(n1, n2)
@@ -19,12 +30,16 @@ def devide(n1, n2)
 end
 
 def root(n)
-  sprintf("%.4f", n ** 0.5)
+  puts sprintf("%.4f", n ** 0.5)
 end
 
+def area(radius)
+  display Math::PI * (radius ** 2)
+end
 
-puts add(5.3333, 2.2346)
-puts minus(5.3333, 2.2346)
-puts mult(5.3333, 2.2346)
-puts devide(5.3333, 2.2346)
-puts root(13)
+add(n1, n2) if oper == 'add'
+minus(n1, n2) if oper == 'minus'
+mult(n1, n2) if oper == 'mult'
+devide(n1, n2) if oper == 'devide'
+root(n1) if oper == 'root'
+area(n1) if oper == 'area'
